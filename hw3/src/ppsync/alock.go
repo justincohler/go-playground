230d57b5ef8c9a5465de487a42fd29f0ac74f648
justincohler <justincohler@gmail.com>
2019-04-22 14:46:09 -0500

    Add saqs.tex, update alock to atomically load/set

--- hw3/src/ppsync/alock.go
+++ hw3/src/ppsync/alock.go
@@ -8,20 +8,20 @@ import (
 // Anderson Queue lock. The zero value for a Anderson lock
 // is an unlocked mutex.
 type ALock struct {
-	Flags []bool
-	slot  int32
-	next  int32
-	N     int32
+	Flags    []bool
+	slot     int32
+	next     int32
+	nThreads int32
 }
 
 // Lock locks lock. If the lock is already in use, the calling goroutine
 // blocks until the lock is available.
 func (lock *ALock) Lock() {
 	slot := atomic.AddInt32(&lock.next, 1)
-	for !lock.Flags[slot%lock.N] {
+	for !lock.Flags[slot%lock.nThreads] {
 	}
-	lock.slot = slot
-	lock.Flags[lock.slot%lock.N] = false
+	atomic.StoreInt32(&lock.slot, slot)
+	lock.Flags[slot%lock.nThreads] = false
 }
 
 // Unlock unlocks lock.
@@ -31,14 +31,14 @@ func (lock *ALock) Lock() {
 // It is allowed for one goroutine to lock a lock and then
 // arrange for another goroutine to unlock it.
 func (lock *ALock) Unlock() {
-	lock.Flags[(lock.slot+1)%lock.N] = true
+	slot := atomic.LoadInt32(&lock.slot)
+	lock.Flags[(slot+1)%lock.nThreads] = true
 }
 
 // NewALock initializes and returns a new ALock
 // for a given number of threads.
 func NewALock(nThreads int) *ALock {
-	lock := &ALock{Flags: make([]bool, nThreads), N: int32(nThreads)}
+	lock := &ALock{Flags: make([]bool, nThreads), nThreads: int32(nThreads)}
 	lock.Flags[0] = true
-
 	return lock
 }
